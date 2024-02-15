35d32c31f0a922e1bd934bf8a767f9e804df591d
PatrykkM <patrykmrzyglod3@gmail.com>
2024-02-15 19:07:18 +0100

    Update

--- src/components/WorkExperienceDesktop.tsx
+++ src/components/WorkExperienceDesktop.tsx
@@ -1,15 +1,46 @@
 import { WorkExperienceProps } from "./WorkExperience";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const WorkExperienceDesktop: React.FC<WorkExperienceProps> = ({
   JobPositions,
 }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const titleVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.3, ease: "easeOut" },
+    },
+  };
   return (
     <section className="bg-light-blue -mt-px p-5 sm:px-10 lg:max-w-7xl m-auto lg:px-16 hidden lg:block dark:bg-light-blue">
       <div className="mt-20  flex flex-col text-slate-50">
         <div className="text-base">What i have done so far</div>
-        <div className="mt-2 text-3xl font-extrabold mb-18 lg:text-6xl">
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={titleVariants}
+          className="mt-2 text-3xl font-extrabold mb-18 lg:text-6xl"
+        >
           Work Experience
-        </div>
+        </motion.div>
         {JobPositions.map((position, id) => (
           <div
             className="flex w-full   rounded-md  justify-center z-50"
