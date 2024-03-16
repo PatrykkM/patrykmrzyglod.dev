77ca13e5b62477f5f325edfe242829ace4facb07
PatrykkM <patrykmrzyglod3@gmail.com>
2024-03-16 20:16:16 +0100

    Update

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,6 +1,20 @@
+import emailjs from "emailjs-com";
 import { motion } from "framer-motion";
 
+import { useRef, useState } from "react";
+
 const ContactMe = () => {
+	const formRef = useRef<HTMLFormElement>(null);
+	const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID as string;
+	const USER_ID = import.meta.env.VITE_EMAILJS_USER_ID as string;
+	const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string;
+
+	const [actionForm, setActionForm] = useState({
+		error: false,
+		correct: false,
+	});
+	const [processing, setProcessing] = useState(false);
+
 	const formVariants = {
 		offscreen: {
 			x: -100,
@@ -16,7 +30,30 @@ const ContactMe = () => {
 			},
 		},
 	};
-
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (formRef.current) {
+			setProcessing((prev) => !prev); //Będzie TRUE czyli błąd bedzie aktwyny Procesuje sie
+			setTimeout(() => {
+				emailjs
+					.sendForm(TEMPLATE_ID, TEMPLATE_ID, formRef.current!, USER_ID)
+					.then(() => {
+						setActionForm((prev) => ({
+							...prev,
+							correct: !prev.correct,
+						}));
+						formRef.current?.reset();
+					})
+					.catch(() => {
+						setActionForm((prev) => ({
+							...prev,
+							error: !prev.error,
+						}));
+					});
+				setProcessing((prev) => !prev);
+			}, 2000);
+		}
+	};
 	return (
 		<section className="text-white lg:w-128" id="Contact">
 			<motion.div
@@ -42,36 +79,50 @@ const ContactMe = () => {
 					</a>
 					or the form provided below.
 				</div>
-				<form action="" className="font-me mt-5 dark:text-gray-800">
+				<form
+					className="font-me mt-5 dark:text-gray-800"
+					onSubmit={(e) => sendEmail(e)}
+					ref={formRef}
+				>
 					<div className="mt-8">
-						<p>Your Name</p>
+						<label htmlFor="from_name">Your Name</label>
 						<input
 							type="text"
 							className="mt-3 w-full  rounded-lg bg-dynamic-menu px-3 py-4 dark:bg-light-mode-items"
 							placeholder="What's your name"
+							id="from_name"
+							name="from_name"
 						/>
 					</div>
 					<div className="mt-8">
-						<p>Your Email</p>
+						<label htmlFor="from_email">Your Email</label>
 						<input
 							type="email"
+							name="from_email"
+							id="from_email"
 							className="mt-3 w-full  rounded-lg bg-dynamic-menu px-3 py-4 dark:bg-light-mode-items"
 							placeholder="What's your email"
 						/>
 					</div>
 					<div className="mt-8">
-						<p>Your Message</p>
+						<label htmlFor="message">Your Message</label>
 						<textarea
 							className="mt-3 flex w-full items-start justify-start rounded-lg bg-dynamic-menu px-3 py-4 dark:bg-light-mode-items"
 							placeholder="What's your message"
 							rows={10}
+							name="message"
+							id="message"
 						/>
 					</div>
 					<button
 						type="submit"
-						className="mt-7 rounded-2xl bg-light-blue px-7 py-3 font-medium dark:text-white "
+						className="w-22 mt-7 flex h-12 w-24 items-center  justify-center rounded-2xl bg-light-blue font-medium  dark:text-white"
 					>
-						Send
+						{processing ? (
+							<div className="border-Light-Green ml-2 h-4 w-4 animate-spin rounded-full border-2 border-t-2 border-t-transparent"></div>
+						) : (
+							<p>Send</p>
+						)}
 					</button>
 				</form>
 			</motion.div>
